89e8142374a1672f22a0deb4c0992583af788664
Padraig Cunningham <padraig.cunningham@ucd.ie>
2013-04-15 19:41:53 +0100

    First commit

---
+++ GitExApril2012/src/GitExApril2012.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : GitExApril2012.c
+ Author      : P‡draig Cunningham
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Git April 2013 Ex V1"); /* prints Git stuff */
+	return EXIT_SUCCESS;
+}
